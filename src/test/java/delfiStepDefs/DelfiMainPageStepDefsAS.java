1d95da1220fa1466cf96e9f976c505cea57ca311
andre <and4rean@inbox.lv>
2017-11-07 23:34:37 +0200

    DelfiAS.feature (Cucumber stuff)

--- src/test/java/delfiStepDefs/DelfiMainPageStepDefsAS.java
+++ src/test/java/delfiStepDefs/DelfiMainPageStepDefsAS.java
@@ -4,6 +4,7 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import delfiPageObjectTestAS.Pages.BaseFunctions;
 import delfiPageObjectTestAS.Pages.HomePage;
 import delfiPageObjectTestAS.Pages.HomePageMOB;
@@ -25,18 +26,25 @@ public class DelfiMainPageStepDefsAS {
 
     private static final String HOME_PAGE_URL = "http://delfi.lv";
     private static final String MOB_HOME_PAGE_URL = "http://m.delfi.lv";
-    //private static /*final*/ String SEARCH_NAME = "Saūda Arābijā aizturēti"; //"Zatlers Šlesera sacīto"; //"Šveices policija"; //"Riga vs Riga";
+    private static /*final*/ String SEARCH_NAME = "Saūda Arābijā aizturēti"; //"Zatlers Šlesera sacīto"; //"Šveices policija"; //"Riga vs Riga";
 
-    /*
-    @Before("Driver, Mob Driver load and Homepage, MOB Homepage load (.*)")
-    public void driver__homepage_load() {
+
+    @Before
+    //@Before("Drivers load (.*)")
+    public void drivers_load() {
         baseFunc.goToURL(HOME_PAGE_URL);
         baseFuncMOB.goToURL(MOB_HOME_PAGE_URL);
     }
-    */
 
-    @Given("Search for Title, MOB title and CommentCount, MOB CommentCount (.*)")
-    public void title_commentCount_search(String SEARCH_NAME) {
+/*
+    @Given("Give search name")
+    public void give_search(String SEARCH_BY) {
+        this.SEARCH_NAME = SEARCH_BY;
+    }
+*/
+
+    @When("Search for Title and CommentCount (.*)")
+    public void title_search(String SEARCH_NAME) {
 
         title = homePage.getArticleTitleNameAS(SEARCH_NAME);
         System.out.println("title AS: "+title);
@@ -50,7 +58,6 @@ public class DelfiMainPageStepDefsAS {
         countMOB = homePageMOB.getArticleCommentCountAS(SEARCH_NAME);
         System.out.println("MOB count AS: "+countMOB);
     }
-
 
     @Then("Comparing (Assert) Title and MOB title (.*)")
     public void assertion_title_test(String title, String titleMOB) {
@@ -67,13 +74,14 @@ public class DelfiMainPageStepDefsAS {
     }
 
 
-/*
-    @After("Quiting driver and MOB driver  (.*)")
+
+    @After
+    //@After("Quiting driver and MOB driver  (.*)")
     public void quit_drivers(){
         baseFunc.quitDriver();
         baseFuncMOB.quitDriver();
     }
-*/
+
 
 
 }
