924e388b36b74ecbb709989e8c5c7c843c4a1f75
andre <and4rean@inbox.lv>
2017-11-14 22:27:48 +0200

    DelfiAS.feature (Cucumber stuff) 2
    http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
    
    test

--- src/test/java/delfiStepDefs/WeatherStepDefs.java
+++ src/test/java/delfiStepDefs/WeatherStepDefs.java
@@ -1,5 +1,6 @@
 package delfiStepDefs;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -11,6 +12,7 @@ import java.math.BigDecimal;
 
 public class WeatherStepDefs {
 
+    //http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
     private WeatherRequester wReq = new WeatherRequester();
     private String cityName;
     private WeatherModel weaModel = new WeatherModel();
@@ -22,7 +24,7 @@ public class WeatherStepDefs {
 
     @When("Requesting weather information")
     public void requestWeatherInformation() throws IOException {
-
+        //http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
         weaModel = wReq.getWeather(cityName);
     }
 
@@ -30,7 +32,50 @@ public class WeatherStepDefs {
     public void checkCoordinates(BigDecimal lon, BigDecimal lat) {
         Assert.assertEquals(lon, weaModel.getCoord().getLon());
         Assert.assertEquals(lat, weaModel.getCoord().getLat());
+    }
 
+
+    @And("Then base is: (.*)")
+    public void checkBase(String base) {
+        Assert.assertEquals(base, weaModel.getBase());
+    }
+
+    @And("Then visibility is: (.*)")
+    public void checkVisibility(String visibility) {
+        Assert.assertEquals(visibility, weaModel.getVisibility());
+    }
+
+    @And("Then winds details are speed: (.*) and deg: (.*)")
+    public void checkWind(BigDecimal speed, Integer deg) {
+        Assert.assertEquals(speed, weaModel.getWind().getSpeed());
+        Assert.assertEquals(deg, weaModel.getWind().getDeg());
+    }
+
+    @And("Then clouds is all: (.*)")
+    public void checkClouds(Integer all) {
+        Assert.assertEquals(all, weaModel.getClouds().getAll());
+    }
+
+
+
+    @And("Then dt is: (.*)")
+    public void checkDt(Integer dt) {
+        Assert.assertEquals(dt, weaModel.getDt());
+    }
+
+    @And("Then id is: (.*)")
+    public void checkId(Integer id) {
+        Assert.assertEquals(id, weaModel.getId());
+    }
+
+    @And("Then name is: (.*)")
+    public void checkDt(String name) {
+        Assert.assertEquals(name, weaModel.getName());
+    }
+
+    @And("Then cod is: (.*)")
+    public void checkCod(Integer cod) {
+        Assert.assertEquals(cod, weaModel.getCod());
     }
 
 
