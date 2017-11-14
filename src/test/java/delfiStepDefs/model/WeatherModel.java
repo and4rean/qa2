15e39b6812acbedf8b454b699422615874618c56
andre <and4rean@inbox.lv>
2017-11-14 22:27:07 +0200

    DelfiAS.feature (Cucumber stuff) 2
    http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
    
    test

--- src/test/java/delfiStepDefs/model/WeatherModel.java
+++ src/test/java/delfiStepDefs/model/WeatherModel.java
@@ -9,12 +9,97 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class WeatherModel {
 
     private Coordinates coord;
+    //private Weather weather;
+    private String base;
+    private String visibility;
+    private Wind wind;
+    private Clouds clouds;
+
+    private Integer dt;
+    private Integer id;
+    private String name;
+    private Integer cod;
+
+
+
+
 
     public Coordinates getCoord() {
         return coord;
     }
-
     public void setCoord(Coordinates coord) {
         this.coord = coord;
     }
+
+/*
+    public Weather getWeather() {
+        return weather;
+    }
+    public void setWeather(Weather weather) { this.weather = weather; }
+*/
+
+    public String getBase() {
+        return base;
+    }
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+
+    public String getVisibility() {
+        return visibility;
+    }
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+
+    public Wind getWind() {
+        return wind;
+    }
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+    public void setClouds(Clouds clouds) {
+        this.clouds = clouds;
+    }
+
+
+    public Integer getDt() {
+        return dt;
+    }
+    public void setDt(Integer dt) {
+        this.dt = dt;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Integer getCod() {
+        return cod;
+    }
+    public void setCod(Integer cod) {
+        this.cod = cod;
+    }
+
+
 }
